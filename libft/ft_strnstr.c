120e9bfca41ddeecf8098154810ff1f7ee1379a5
caaiopereira <caaiopereira9@gmail.com>
2023-10-24 15:54:15 -0300

    Update]

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: caiperei <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 11:02:12 by caiperei          #+#    #+#             */
-/*   Updated: 2023/10/24 11:13:41 by caiperei         ###   ########.fr       */
+/*   Updated: 2023/10/24 15:45:30 by caiperei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 	size_t	little_len;
 
 	little_len = ft_strlen(little);
-	if (little_len == 0)
+	if (little_len == 0 || len == 0)
 	{
 		return ((char *)big);
 	}
