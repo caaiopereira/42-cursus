95c2db48cdcdcf523feaa96c6b9401cf712ae8ce
caaiopereira <caaiopereira9@gmail.com>
2023-10-24 13:56:08 -0300

    Update]

--- libft/ft_memcmp.c
+++ libft/ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: caiperei <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 09:48:10 by caiperei          #+#    #+#             */
-/*   Updated: 2023/10/24 10:53:24 by caiperei         ###   ########.fr       */
+/*   Updated: 2023/10/24 13:51:51 by caiperei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	str_s1 = (unsigned char *)s1;
 	str_s2 = (unsigned char *)s2;
 	i = 0;
-	while ((str_s1[i] != '\0' || str_s2[i] != '\0') && (i < n))
+	while (i < n)
 	{
 		if (str_s1[i] != str_s2[i])
 		{
