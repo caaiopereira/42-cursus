629ec9ff3dd16e9228764e72e9ff93306b493182
caaiopereira <caaiopereira9@gmail.com>
2023-10-25 15:39:15 -0300

    Add putstr_fd

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: caiperei <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 15:37:26 by caiperei          #+#    #+#             */
-/*   Updated: 2023/10/25 15:28:55 by caiperei         ###   ########.fr       */
+/*   Updated: 2023/10/25 15:30:45 by caiperei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,7 +47,7 @@ char	*ft_strtrim(char const *s1, char const *set);
 char	*ft_strmapi(char const *s, char(*f)(unsigned int, char));
 void	ft_striteri(char *s, void(*f)(unsigned int, char*));
 void	ft_putchar_fd(char c, int fd);
-//void	ft_putstr_fd(char *s, int fd);
+void	ft_putstr_fd(char *s, int fd);
 //void	ft_putendl_fd(char *s, int fd);
 //void	ft_putnbr_fd(int n, int fd);
 
