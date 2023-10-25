9efbf3c047bf795b56c54fd00701e9c0ea082e21
caaiopereira <caaiopereira9@gmail.com>
2023-10-25 16:06:15 -0300

    Add new file putendl_fd

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: caiperei <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 15:37:26 by caiperei          #+#    #+#             */
-/*   Updated: 2023/10/25 15:40:51 by caiperei         ###   ########.fr       */
+/*   Updated: 2023/10/25 15:57:26 by caiperei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,7 +48,7 @@ char	*ft_strmapi(char const *s, char(*f)(unsigned int, char));
 void	ft_striteri(char *s, void(*f)(unsigned int, char*));
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
-//void	ft_putendl_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
 
 #endif
