5ceea196730a389c650bc3bea00a87383116a695
caaiopereira <caaiopereira9@gmail.com>
2023-11-06 10:19:19 -0300

    fix update

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -6,39 +6,30 @@
 /*   By: caiperei <caiperei@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/30 16:16:36 by caiperei          #+#    #+#             */
-/*   Updated: 2023/10/31 14:33:33 by caiperei         ###   ########.fr       */
+/*   Updated: 2023/11/06 08:16:21 by caiperei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*
-	Concatena duas strings
-*/
 #include "libft.h"
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	char	*result;
-	size_t	i;
-	size_t	x;
+	int		lens1;
+	int		lens2;
+	char	*join;
 
-	i = 0;
-	x = 0;
-	if (!s1 || !s2)
-		return (0);
-	result = (char *)malloc(ft_strlen(s1) + ft_strlen(s2 + 1));
-	if (result == 0)
-		return (0);
-	while (s1[i] != '\0')
-	{
-		result[i] = s1[i];
-		i++;
-	}
-	while (s2[x] != '\0')
-	{
-		result[i] = s2[x];
-		i++;
-		x++;
-	}
-	result[i] = '\0';
-	return (result);
+	if (!s1 && !s2)
+		return (ft_strdup(""));
+	if (s1 && !s2)
+		return (ft_strdup(s1));
+	if (!s1 && s2)
+		return (ft_strdup(s2));
+	lens1 = ft_strlen(s1);
+	lens2 = ft_strlen(s2);
+	join = malloc(sizeof(char) * (lens1 + lens2 + 1));
+	if (join == NULL)
+		return (NULL);
+	ft_strlcpy(join, s1, lens1 + 1);
+	ft_strlcat(join, s2, lens1 + lens2 + 1);
+	return (join);
 }
