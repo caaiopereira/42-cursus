a056fbf2a6c1617ff9f08b62cc16162d90971ef2
caaiopereira <caaiopereira9@gmail.com>
2023-11-21 09:40:43 -0300

    update gnl.h

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,13 +6,24 @@
 /*   By: caiperei <caiperei@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 13:52:49 by caiperei          #+#    #+#             */
-/*   Updated: 2023/11/16 13:55:20 by caiperei         ###   ########.fr       */
+/*   Updated: 2023/11/21 09:40:18 by caiperei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+# define BUFFER_SIZE 20
+# endif
+
 char	*get_next_line(int fd);
+char	*ft_strdup(char const *s);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char *s1, char *s2);
+size_t	ft_strlen(char const *str);
 
 #endif
