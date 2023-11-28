386aef38b51567a685430bf65a5003841c7d7a7f
caaiopereira <caaiopereira9@gmail.com>
2023-11-28 09:42:30 -0300

    update gnl.c

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -12,126 +12,93 @@
 
 #include "get_next_line.h"
 
-//atualiza o conteudo apos a primeira linha lida
 static char	*rest_line(char *line)
 {
-	size_t	i;//percorrer string
-	char	*rest;//armazenar conteudo apos \n
+	size_t	i;
+	char	*rest;
 	size_t	count;
 
 	i = 0;
-	while (line[i] && line[i] != '\n')//calcula o tamanho de line 
+	while (line[i] && line[i] != '\n')
 		i++;
-	if (!(line[i]))//verifica se caractere atual é o \0
+	if (!(line[i]))
 	{
-		free(line);// libera a memoria
-		return (NULL);// caso nao há mais conteudo retornar null
+		free(line);
+		return (NULL);
 	}
-	if (line[i] == '\n')//verifica se é o \n
-		i++;//incrementa para o prox caractere apos \n
+	if (line[i] == '\n')
+		i++;
 	rest = malloc(sizeof(char) * ft_strlen(line) - i + 1);
-	//rest recebe tamanho total de line, subtrai (i) e +1 para nulo no final
-	//para calcular tamanho da alocação
 	if (!(rest))
-		return (NULL);//verificação de memoria valida, caso nao retorna null
+		return (NULL);
 	count = 0;
-	while (line[i])//line começa a copiar apartir da posição onde foi diminuido de i
-		rest[count++] = line[i++];//copia line para rest
+	while (line[i])
+		rest[count++] = line[i++];
 	rest[count] = '\0';
-	free(line);//liberação de memória
-	return (rest);//retorna a linha apos o \n
+	free(line);
+	return (rest);
 }
 
-//corta e retorna a primeira linha
 static char	*cut_line(char *line)
 {
-	size_t	i;//percorrer string
-	char	*cut;//armazenar linha cortada
+	size_t	i;
+	char	*cut;
 
 	i = 0;
-	if (!*line)//verificação se line esta vazia
-		return (NULL);//retornar null caso não há nada para cortar
-	while (line[i] && line[i] != '\n')//percorre até o \n
+	if (!*line)
+		return (NULL);
+	while (line[i] && line[i] != '\n')
 		i++;
-	cut = malloc(sizeof(char) * i + 2);//aloca na memoria, +2 para \n e o \0
+	cut = malloc(sizeof(char) * i + 2);
 	if (!(cut))
-		return (NULL);//verificação de memoria valida, caso nao retorna null
+		return (NULL);
 	i = 0;
 	while (line[i] && line[i] != '\n')
 	{
-		cut[i] = line[i];//copia line para cut
-		i++;//incrementa até encontrar \n ou \0 da string
+		cut[i] = line[i];
+		i++;
 	}
 	if (line[i] == '\n')
-		cut[i++] = '\n';//adiciona \n
-	cut[i] = '\0';//adiciona nulo indicando fim da string
-	return (cut);//retorna a linha cortada
+		cut[i++] = '\n';
+	cut[i] = '\0';
+	return (cut);
 }
 
-//ler e guarda o conteudo
 static char	*read_line(char *line, int fd)
 {
-	char	*buffer;//armazenar linha
-	int		count;//guardar numero de bytes lidos
+	char	*buffer;
+	int		count;
 
 	count = 1;
 	buffer = malloc(BUFFER_SIZE + 1);
-	//aloca na memoria com tamanho especificado(BUFFER_SIZE), +1 para o \0
 	if (!(buffer))
-		return (NULL);//verificação de memoria valida, caso nao retorna null
+		return (NULL);
 	while (count > 0 && !ft_strchr(line, '\n'))
-	{//loop caso nao encontre \n
+	{
 		count = read(fd, buffer, BUFFER_SIZE);
-		//ler o conteudo em blocos adicionando ao buffer
 		if (count < 0)
-		//verificação de erro durante a leitura
 		{
-			free(buffer);//liberação de memoria
+			free(buffer);
 			return (NULL);
 		}
-		buffer[count] = '\0';//adiciona nulo
+		buffer[count] = '\0';
 		line = ft_strjoin(line, buffer);
-		//concatena o buffer com line indicando como dados lidos
 	}
-	free(buffer);//liberação de memoria apos a leitura
-	return (line);//retorna line contendo os dados lidos até o momento
+	free(buffer);
+	return (line);
 }
 
-//ler linhas de um arquivo uma por vez
 char	*get_next_line(int fd)
 {
-	static char	*string;//variavel estatica para manter conteudo
+	static char	*string;
 	char		*line;
 
-	if (fd < 0 || BUFFER_SIZE <= 0)//verifica se são valores positivos
+	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	string = read_line(string, fd);//ler e guarda o conteudo
+	string = read_line(string, fd);
 	if (!(string))
-		return (NULL);//verifica se houve falha ou chegou no final do arquivo
-	line = cut_line(string);//corta e retorna a primeira linha
-	string = rest_line(string);//atualiza o conteudo apos a primeira linha lida
-	return (line);// retorna a linha que foi lida
+		return (NULL);
+	line = cut_line(string);
+	string = rest_line(string);
+	return (line);
 }
-
-/*
-a\n 	i = 2
-b\n
-c\0
-
-i = 2+1
-i = 3
-
-rest = (6) - i (3) + 1
-
-rest = (4)
-
-rest[count](4) = line[i](3)
-
-rest[count] = b\n
-	       c
-
-rest[count] = \0
-
-rest = b\n
- 	c\0
- */
