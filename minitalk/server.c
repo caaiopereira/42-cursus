998f4e783dec22019e8e0ba6154615c78fa76963
caaiopereira <caaiopereira9@gmail.com>
2024-02-12 15:57:03 -0300

    update server.c

--- minitalk/server.c
+++ minitalk/server.c
@@ -1,20 +1,20 @@
 #include "minitalk.h"
 
-void	print_bits(int bit)
+void	print_bit(int bit)
 {
 	static int				i;
-	static unsigned char	x;
+	static unsigned char	c;
 
 	if (bit == SIGUSR2)
-		x = x << 1;
+		c = c << 1;
 	else if (bit == SIGUSR1)
-		x = (x << 1) | 0b00000001;
+		c = (c << 1) | 0b00000001;
 	i++;
 	if (i == 8)
 	{
-		ft_printf("%x", x);
+		ft_printf("%c", c);
 		i = 0;
-		x = 0;
+		c = 0;
 	}
 }
 
@@ -23,8 +23,8 @@ int	main(void)
 	ft_printf("\t Server PID: %d\n", getpid());
 	while (1)
 	{
-		signal(SIGUSR1, print_bits);
-		signal(SIGUSR2, print_bits);
+		signal(SIGUSR1, print_bit);
+		signal(SIGUSR2, print_bit);
 		pause();
 	}
 	return (0);
