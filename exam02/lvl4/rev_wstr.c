8a4359d7c317e21915d2bbbcbd9cafdf23c8d55f
caaiopereira <caaiopereira9@gmail.com>
2024-03-12 13:09:16 -0300

    rev_wstr update

--- exam02/lvl4/rev_wstr.c
+++ exam02/lvl4/rev_wstr.c
@@ -24,20 +24,20 @@ int main(int ac, char **av)
 			i++;
 		while(i >= 0)
 		{
-			while( av[1][i] == '\0' || av[1][i] == ' ' || av[1][i] == '\t')
+			while(av[1][i] == '\0' || av[1][i] == ' ' || av[1][i] == '\t')
 				i--;
 			end = i;
 			while(av[1][i] && av[1][i] != ' ' && av[1][i] != '\t')
 				i--;
 			start = i + 1;
-			int  flag;
-			flag = start;
-			while(start <= end)
+			int  flg;
+			flg = start;
+			while(end >= start)
 			{
 				write (1, &av[1][start],1);
 				start++;		
 			}
-			if (flag != 0)
+			if (flg != 0)
 			{
 				write(1, " ", 1);
 			}
